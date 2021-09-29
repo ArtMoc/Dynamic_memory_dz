3871bc5bc07fee5ce8641d0ea05b107d5b5b8d35
ArtMoc <tem.mochalov@yandex.ru>
2021-09-29 14:13:13 +0300

    DONE

--- Dynamic_memory_dz/main.cpp
+++ Dynamic_memory_dz/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+#define DYNAMIC_MEMORY_1
+//#define DYNAMIC_MEMORY_2
 
 void FillRand(int arr[], const unsigned int n);
 void Print(int arr[], const unsigned int n);
@@ -7,14 +13,29 @@ void Print(int arr[], const unsigned int n);
 void push_back(int*& arr, int& n, int b_value);
 void push_front(int*& arr, int& n, int f_value);
 void insert(int*& arr, int& n, int i_value, int i_index);
-
 void pop_back(int*& arr, int& n);
 void pop_front(int*& arr, int& n);
 void erase(int*& arr, int& n, int e_index);
 
+//////////////////////////////////
+
+void FillRand(int** arr, const unsigned int rows, const unsigned int cols);
+void Print(int** arr, const unsigned int rows, const unsigned int cols);
+
+int** allocate(const unsigned int rows, const unsigned int cols);
+void clear(int** arr, const unsigned int rows);
+
+int** push_row_back(int** arr, unsigned int& rows, const unsigned int cols);
+int** push_row_front(int** arr, unsigned int& rows, const unsigned int cols);
+int** insert_row(int** arr, unsigned int& rows, const unsigned int cols, const unsigned int i_index);
+int** pop_row_back(int** arr, unsigned int& rows, const unsigned int cols);
+int** pop_row_front(int** arr, unsigned int& rows, const unsigned int cols);
+int** erase_row(int** arr, unsigned int& rows, const unsigned int cols, unsigned int e_index);
+
 void main()
 {
 	setlocale(LC_ALL, "Rus");
+#ifdef DYNAMIC_MEMORY_1
 	int n;
 	int b_value;
 	int f_value;
@@ -46,6 +67,50 @@ void main()
 	erase(arr, n, e_index);
 	Print(arr, n);
 	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+	unsigned int rows, cols;
+	cout << "Введите количество строк: ";cin >> rows;
+	cout << "Введите количество столбцов: ";cin >> cols;
+	int** arr = allocate(rows, cols);
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n-------------------------------\n";
+	arr = push_row_back(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n-------------------------------\n";
+	arr = push_row_front(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n-------------------------------\n";
+	int i_index;
+	cout << "Enter the index where to insert the row: "; cin >> i_index;
+	arr = insert_row(arr, rows, cols, i_index);
+	Print(arr, rows, cols);
+	cout << "\n-------------------------------\n";
+	arr = pop_row_back(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n-------------------------------\n";
+	arr = pop_row_front(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n-------------------------------\n";
+	int e_index;
+	cout << "Enter the index of the row to delete: "; cin >> e_index;
+	arr = erase_row(arr, rows, cols, e_index);
+	Print(arr, rows, cols);
+	cout << "\n-------------------------------\n";
+	//Вывод массива через арифметику указателей и оператор разыменования
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << *(*(arr + i) + j) << "\t";
+		}
+		cout << endl;
+	}
+	clear(arr, rows);
+#endif // DYNAMIC_MEMORY_2
+
 }
 
 
@@ -106,7 +171,6 @@ void insert(int*& arr, int& n, int i_value, int i_index)
 	arr[i_index] = i_value;
 	n++;
 }
-
 void pop_back(int*& arr, int& n)
 {
 	int* buffer = new int[n];
@@ -136,4 +200,125 @@ void erase(int*& arr, int& n, int e_index)
 		arr[i] = arr[i + 1];
 	}
 	n--;
+}
+
+void FillRand(int** arr, const unsigned int rows, const unsigned int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+void Print(int** arr, const unsigned int rows, const unsigned int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << '\t';
+		}
+		cout << endl;
+	}
+}
+
+int** allocate(const unsigned int rows, const unsigned int cols)
+{
+	//1)добавляем указатель на указатель, и сохраняем в него адрес массива указателей:
+	int** arr = new int* [rows];
+	//2)создаем строки двумерного динамического массива:
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols] {};
+	}
+	return arr;
+}
+void clear(int** arr, const unsigned int rows)
+{
+	//3)удаляем строки 
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	//4)удаляем массив указателей
+	delete[] arr;
+}
+
+int** push_row_back(int** arr, unsigned int& rows, const unsigned int cols)
+{
+	int** buffer = new int* [rows + 1]{};
+	for (int i = 0; i < rows; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[rows] = new int [cols] {};
+	rows++;
+	return arr;
+}
+int** push_row_front(int** arr, unsigned int& rows, const unsigned int cols)
+{
+	int** buffer = new int* [rows + 1]{};
+	for (int i = 0; i < rows; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[0] = new int [cols] {};
+	rows++;
+	return arr;
+}
+int** insert_row(int** arr, unsigned int& rows, const unsigned int cols, const unsigned int i_index)
+{
+	int** buffer = new int* [rows + 1]{};
+	for (int i = 0; i < rows; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	for (int i = rows - 1; i >= i_index; i--)
+	{
+		arr[i + 1] = arr[i];
+	}
+	arr[i_index] = new int[cols] {};
+	rows++;
+	return arr;
+}
+int** pop_row_back(int** arr, unsigned int& rows, const unsigned int cols)
+{
+	int** buffer = new int* [rows] {};
+	for (int i = 0; i < rows; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	rows--;
+	return arr;
+}
+int** pop_row_front(int** arr, unsigned int& rows, const unsigned int cols)
+{
+	int** buffer = new int* [rows] {};
+	for (int i = 0; i < rows; i++)
+	{
+		buffer[i] = arr[i + 1];
+	}
+	delete[] arr;
+	arr = buffer;
+	rows--;
+	return arr;
+}
+int** erase_row(int** arr, unsigned int& rows, const unsigned int cols, unsigned int e_index)
+{
+	for (int i = e_index; i < rows - 1; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+	rows--;
+	return arr;
 }
